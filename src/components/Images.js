02a7af65c63f0354086d5d3dcfd752b5bff5f2fe
nathanziarczyk <nathanziarczyk1997@outlook.com>
2020-08-08 18:42:04 +0200

    open afbeelding in nieuw blad knop

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,17 +1,27 @@
-import React from "react";
-import { useFirestore } from "../hooks/useFirestore";
+import React, { useContext } from "react";
 import { motion } from "framer-motion";
+import { BsFillTrashFill } from "react-icons/bs";
+import { FiExternalLink } from "react-icons/fi";
+
+import { useFirestore } from "../hooks/useFirestore";
+import { UserContext } from "../context/user";
 
 export default function Images({ setImage }) {
   const { docs } = useFirestore("images");
+  const { user } = useContext(UserContext);
+  console.log(user.uid);
   return (
     <div className="container images-grid">
       {docs.map((doc) => {
+        console.log(doc);
         return (
           <motion.div
             className="images-grid-item"
             key={doc.id}
-            onClick={(e) => setImage(doc.url)}
+            onClick={(e) => {
+              if (e.target.classList.contains("trash-icon")) return null;
+              setImage(doc.url);
+            }}
             whileHover={{ opacity: 1 }}
             layout
           >
@@ -20,8 +30,13 @@ export default function Images({ setImage }) {
               alt="dat van iets"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              transition={{ delay: 1 }}
             />
+            {/* {user.uid === doc.user.id && (
+              <BsFillTrashFill className="trash-icon" />
+            )} */}
+            <a href={doc.url} target="_blank" rel="noopener noreferrer">
+              <FiExternalLink className="trash-icon" />
+            </a>
           </motion.div>
         );
       })}
